1b94576add022eb398bd962a20b77b9b239efcf4
Jaafar Wannous <jaafarwannous1@gmail.com>
2024-05-21 04:35:04 +0300

    initial commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const enviroment = {
+  production: true,
+  firebaseApiKey: 'AIzaSyBFHYv7ofAyGjftVWJLkl6qV3ONyAm--8c'
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const enviroment = {
+  production: false,
+  firebaseApiKey: 'AIzaSyBFHYv7ofAyGjftVWJLkl6qV3ONyAm--8c'
+}
